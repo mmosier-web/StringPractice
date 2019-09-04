ecd8df6892f27814a5f7128b94722ddb6c916444
mmosier-web <mmosier5@gmail.com>
2019-09-04 14:01:21 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StringPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
